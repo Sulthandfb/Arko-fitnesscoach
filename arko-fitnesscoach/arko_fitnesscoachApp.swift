1a35a74e21aab5fda24f4fa5ece28e3c888980c3
Sunraku <sulthanraihan701@gmail.com>
2026-05-07 09:39:00 +0800

    Initial Commit

---
+++ arko-fitnesscoach/arko_fitnesscoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  arko_fitnesscoachApp.swift
+//  arko-fitnesscoach
+//
+//  Created by 20 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct arko_fitnesscoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
